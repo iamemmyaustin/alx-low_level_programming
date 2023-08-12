93094d349a60ee740e7fc9c5d68210fb177e890f
iamemmyaustin <emmanuelaugustine1991@gmail.com>
2023-08-12 02:34:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printsf function
+ *
+ *  Return: Success (0)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
